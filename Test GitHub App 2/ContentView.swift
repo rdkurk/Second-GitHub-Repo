be4e52ca48df82fb5a363141fdd5e08e2a605866
Richard Kurk <rdkurk@msn.com>
2022-11-09 14:18:47 -0800

    Initial Commit

---
+++ Test GitHub App 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test GitHub App 2
+//
+//  Created by Richard Kurk on 11/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test GitHub App 2/Test_GitHub_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GitHub_App_2App.swift
+//  Test GitHub App 2
+//
+//  Created by Richard Kurk on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_GitHub_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
